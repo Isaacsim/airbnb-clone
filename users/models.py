0daa17e7d4736dfeb2ce5e9d3fc76a9ef586aa34
Isaacsim <dltkr34@naver.com>
2020-06-11 20:59:47 +0900

    last

---
+++ users/models.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# Create your models here.
+class User(AbstractUser):
+
+    """ Custom User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "male"),
+        (GENDER_FEMALE, "female"),
+        (GENDER_OTHER, "other"),
+    )
+
+    LANGUAGE_ENGLISH = "EN"
+    LANGUAGE_KOREAN = "KR"
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KOREAN, "Korean"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_KRW, "KRW"),
+    )
+
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
+    bio = models.TextField(default="", blank=True)  # null=TURE 비어있어도 상관없다
+    birthdate = models.DateField(blank=True, null=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
+
+    superhost = models.BooleanField(default=False)
