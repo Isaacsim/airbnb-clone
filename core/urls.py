b9631286417d8e1d3b43239b05dafdcf1f6d7e68
Isaacsim <dltkr34@naver.com>
2020-06-15 00:48:24 +0900

    #11.end

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from rooms import views as room_views
 
 app_name = "core"
 
+# path 의 arg는 URL과 function만 가능하다
 urlpatterns = [
-    path("", room_views.all_rooms, name="home"),
+    path("", room_views.HomeView.as_view(), name="home"),
 ]
