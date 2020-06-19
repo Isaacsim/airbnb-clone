37c1d1819db86a614cb382bf639f8fb97be943d2
Isaacsim <dltkr34@naver.com>
2020-06-19 16:31:38 +0900

    #14.end

--- users/views.py
+++ users/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render, redirect, reverse
+from django.urls import reverse_lazy
 from django.views import View
+from django.views.generic import FormView
 from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 # Create your views here.
-class LoginView(View):
+class LoginView(FormView):
+
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
+    success_url = reverse_lazy("core:home")
+    initial = {"email": "isaac@sim.com"}
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+
+    """
     def get(self, request):
         form = forms.LoginForm(initial={"email": "isaac@sim.com"})
         return render(request, "users/login.html", {"form": form})
@@ -18,8 +35,8 @@ class LoginView(View):
             if user is not None:
                 login(request, user)
                 return redirect(reverse("core:home"))
-
         return render(request, "users/login.html", {"form": form})
+    """
 
 
 def log_out(request):
